e97f1671b082e9cdb3632b61b79eb8045590fdfd
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-04-12 19:19:50 +0600

    Rounter debuging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,22 @@ const Header = () => {
     <>
         <header className=' w-full flex items-center justify-center absolute top-0 left-0 bg-[#0c192c] py-6'>
             <ul className="flex text-white text-sm uppercase font-semibold gap-4">
-                <li className="py-2 cursor-pointer px-4 rounded-[5px] transition-all group hover:bg-[#3f4042]">
-                    <Link to='/'><FaHome  className="h-[20px] w-[20px] group-hover:text-violet-700 transition-all group-hover:text-shadow-[0_0_10px_#7c3aed]"/></Link>
+                <li >
+                    <Link className="py-2 inline-block cursor-pointer px-4 rounded-[5px] transition-all group hover:bg-[#3f4042]" to='/'>
+                    <FaHome  className="h-[20px] w-[20px] group-hover:text-violet-700 transition-all group-hover:text-shadow-[0_0_10px_#7c3aed]"/>
+                    </Link>
                 </li>
                
-                <li className="py-2 cursor-pointer px-4 rounded-[5px] transition-all group hover:bg-[#3f4042]">
-                    <Link to='todol-list'><IoIosListBox  className="h-[20px] w-[20px] group-hover:text-violet-700 transition-all group-hover:text-shadow-[0_0_10px_#7c3aed]"/></Link>
+                <li >
+                    <Link className="py-2 inline-block cursor-pointer px-4 rounded-[5px] transition-all group hover:bg-[#3f4042]" to='todol-list'>
+                    <IoIosListBox  className="h-[20px] w-[20px] group-hover:text-violet-700 transition-all group-hover:text-shadow-[0_0_10px_#7c3aed]"/>
+                    </Link>
                 </li>
                
-                <li className="py-2 cursor-pointer px-4 rounded-[5px] transition-all group hover:bg-[#3f4042]">
-                    <Link to='number-guess-game'><GiQueenCrown  className="h-[20px] w-[20px] group-hover:text-violet-700 transition-all group-hover:text-shadow-[0_0_10px_#7c3aed]"/></Link>
+                <li >
+                    <Link className="py-2 inline-block cursor-pointer px-4 rounded-[5px] transition-all group hover:bg-[#3f4042]" to='number-guess-game'>
+                    <GiQueenCrown  className="h-[20px] w-[20px] group-hover:text-violet-700 transition-all group-hover:text-shadow-[0_0_10px_#7c3aed]"/>
+                    </Link>
                 </li>
                
             </ul>
